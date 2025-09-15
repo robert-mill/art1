d9a856c8c0872275d3068cdadb3c85b7a91309da
Robert Mill <r.mill@ntlworld.com>
2025-09-15 09:41:12 +0100

    initial commit 22

--- src/app/case-study/[uid]/page.tsx
+++ src/app/case-study/[uid]/page.tsx
@@ -1,28 +1,71 @@
+// import { Metadata } from "next";
+ import { notFound } from "next/navigation";
+ import { PrismicText, SliceZone } from "@prismicio/react";
 
-type ParamProps = {
+ import { createClient } from "@/prismicio";
+ import { components } from "@/slices";
+ import Bounded from "@/components/Bounded";
+ import StarGrid from "@/components/StarGrid";
+ import { PrismicNextImage } from "@prismicio/next";
+ import { asText } from "@prismicio/client";
+
+type Params = {
   params: Promise<{ uid: string }>;
 };
 
-export default async function Page({ params }: ParamProps) {
+export default async function Page({ params }: Params) {
   const uid = (await params).uid;
+  const client = createClient();
+  const page = await client.getByUID("case_study",  uid ).catch(() => notFound());
+
   return (
-    <div>Hi {uid}</div>
+         <Bounded as="article">
+       <div className="relative grid place-items-center text-center">
+         <StarGrid />
+         <h1 className="text-7xl font-medium">
+           <PrismicText field={page.data.company} />
+           <p className="text-lg text-yellow-500">Case Study</p>
+         </h1>
+         <p className="mb-4 mt-8 max-w-xl text-lg text-slate-300">
+           <PrismicText field={page.data.description} />
+         </p>
+         <PrismicNextImage
+           field={page.data.logo_image}
+           className="rounded-lg"
+           quality={100}
+         />
+       </div>
+       <div className="mx-auto">
+         <SliceZone slices={page.data.slices} components={components} />
+       </div>
+     </Bounded>
   );
 
+}
+ export async function generateMetadata({ params }: Params) {
+  const uid = (await params).uid;
+  const client = createClient();
+  const page = await client
+    .getByUID("case_study", uid)
+    .catch(() => notFound());
+
+  return {
+    title: `${page.data.meta_title || asText(page.data.company) + " Case Study"}`,
+    description: page.data.meta_description,
+  };
+}
+
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("case_study");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
 }
 
 
 
-// import { Metadata } from "next";
-// import { notFound } from "next/navigation";
-// import { PrismicText, SliceZone } from "@prismicio/react";
-
-// import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
-// import Bounded from "@/components/Bounded";
-// import StarGrid from "@/components/StarGrid";
-// import { PrismicNextImage } from "@prismicio/next";
-// import { asText } from "@prismicio/client";
 
 // type Params = { uid: string };
 
@@ -54,27 +97,3 @@ export default async function Page({ params }: ParamProps) {
 //   );
 // }
 
-// export async function generateMetadata({
-//   params,
-// }: {
-//   params: Params;
-// }): Promise<Metadata> {
-//   const client = createClient();
-//   const page = await client
-//     .getByUID("case_study", params.uid)
-//     .catch(() => notFound());
-
-//   return {
-//     title: `${page.data.meta_title || asText(page.data.company) + " Case Study"}`,
-//     description: page.data.meta_description,
-//   };
-// }
-
-// export async function generateStaticParams() {
-//   const client = createClient();
-//   const pages = await client.getAllByType("case_study");
-
-//   return pages.map((page) => {
-//     return { uid: page.uid };
-//   });
-// }
